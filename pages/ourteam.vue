5caf0241114249a607810c444b656136da1f4a12
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2024-02-17 13:06:08 +0100

    nuxt3

---
+++ pages/ourteam.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Hello Our Team</p>
+    <p>Hello Our Team</p>
+    <p>Hello Our Team</p>
+    <p>Hello Our Team</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
