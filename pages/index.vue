dda2af84f36a2456128cb7b05cc2c75afbacd707
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2024-02-18 22:02:38 +0100

    Landing page in progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  Home
+  <div class="relative min-h-screen">
+    <img src="../public/hero-image.png" alt="hero" class="object-cover h-full w-full absolute insert-0"/>
+    <div class="absolute inset-0 bg-gradient-to-r from-black/80 via-black/60 to-transparent"></div>
+    <div class="relative h-full container mx-auto">
+      <div class="text-white pt-48 flex flex-col gap-3">
+        <h1 class="capitalize text-6xl font-bold">gasfleet engineering <br /> limited - EPCM company</h1>
+        <p></p>
+      </div>
+      
+    </div>
+
+  </div>
 </template>
 
 <script>
+ // Use "~/" to reference the root directory
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+ 
 }
 </script>
