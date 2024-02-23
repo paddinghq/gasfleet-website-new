aca02b395a3e4b8a5d4d4f2ec71537bc8580006a
Abdulfattah Sheu <elsheikh.saf@gmail.com>
2024-02-23 22:15:07 +0100

    single product page done in a bit

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,16 +1,16 @@
 <script setup>
-import All from "@/components/All.vue";
-import Completed from "@/components/Completed.vue";
-import Ongoing from "@/components/Ongoing.vue";
-import { ref, computed } from "vue";
+import All from '@/components/All.vue';
+import Completed from '@/components/Completed.vue';
+import Ongoing from '@/components/Ongoing.vue';
+import { ref, computed } from 'vue';
 
-const activeTab = ref("management");
+const activeTab = ref('management');
 
 const activeComponent = computed(() => {
   switch (activeTab.value) {
-    case "completed":
+    case 'completed':
       return Completed;
-    case "ongoing":
+    case 'ongoing':
       return Ongoing;
     default:
       return All;
@@ -40,10 +40,12 @@ const activateTab = (tab) => {
       >
         <h1 class="font-bold mt-5 text-5xl">Projects</h1>
         <p class="text-[1.2rem]">
-          We are committed to providing our clients with comprehensive and innovative solutions to the <br /> complex challenges facing the oil and gas industry.
+          We are committed to providing our clients with comprehensive and
+          innovative solutions to the <br />
+          complex challenges facing the oil and gas industry.
         </p>
       </div>
-      
+
       <div class="flex gap-12 items-center justify-center pb-8 pt-6">
         <button
           class="button"
@@ -74,15 +76,28 @@ const activateTab = (tab) => {
     <component :is="activeComponent" />
 
     <div class="flex justify-center my-5">
-
-        <button class="py-2 px-4 text-black border-2 text-xl rounded-xl hover:bg-blue-300 transition-all cursor-pointer">Load More</button>
+      <button
+        class="py-2 px-4 text-black border-2 text-xl rounded-xl hover:bg-blue-300 transition-all cursor-pointer"
+      >
+        Load More
+      </button>
     </div>
 
     <div class="bg-blue-100 mt-5">
       <div class="container mx-auto py-10 text-center">
-        <h2 class="font-bold text-4xl capitalize">Ready to revolutionize your projects? get in touch with us</h2>
-        <p class="w-3/4 mx-auto my-6 text-xl">Unleash the potential of your projects with our pioneering oil and gas engineering solutions. Let's shape the future together. Talk to us about your next groundbreaking project.</p>
-        <button class="py-2 px-4 text-white bg-blue-800 text-xl rounded-xl hover:bg-blue-300 transition-all">Contact us</button>
+        <h2 class="font-bold text-4xl capitalize">
+          Ready to revolutionize your projects? get in touch with us
+        </h2>
+        <p class="w-3/4 mx-auto my-6 text-xl">
+          Unleash the potential of your projects with our pioneering oil and gas
+          engineering solutions. Let's shape the future together. Talk to us
+          about your next groundbreaking project.
+        </p>
+        <button
+          class="py-2 px-4 text-white bg-blue-800 text-xl rounded-xl hover:bg-blue-300 transition-all"
+        >
+          Contact us
+        </button>
       </div>
     </div>
   </div>
