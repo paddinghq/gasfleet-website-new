fbc7b0d875ed1b6e963d9357d6546a00abde4f45
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-02-24 19:55:45 +0100

    dev tweaks

--- components/Header.vue
+++ components/Header.vue
@@ -5,6 +5,7 @@ const NuxtImgSrc1 = ref("/down.png");
 const NuxtImgSrc2 = ref("/down.png");
 const isDropdownOpen1 = ref(false);
 const isDropdownOpen2 = ref(false);
+
 const toggleDropdown1 = () => {
   isDropdownOpen1.value = !isDropdownOpen1.value;
   NuxtImgSrc1.value = isDropdownOpen1.value ? "/down.png" : "/up.png";
@@ -23,29 +24,109 @@ const toggleDropdown2 = () => {
 
     <header>
       <nav class="nav-links">
-        <button class="button" @click="toggleDropdown1">
-          <NuxtLink class="sans text-lg transition-all hover:text-gray-500">Who we are</NuxtLink>
-          <NuxtImg
-            v-bind:src="NuxtImgSrc1"
-            alt="arrow"
-            height="24"
-            width="24"
-          />
-        </button>
-        <button class="button" @click="toggleDropdown2">
-          <NuxtLink class="sans text-lg transition-all hover:text-gray-500">What we do</NuxtLink>
-          <NuxtImg
-            v-bind:src="NuxtImgSrc2"
-            alt="arrow"
-            height="24"
-            width="24"
-          />
+        <div class="relative">
+          <button @click="toggleDropdown1" class="button">
+            <NuxtLink class="sans text-lg transition-all hover:text-gray-500"
+              >Who we are</NuxtLink
+            >
+            <NuxtImg
+              :src="NuxtImgSrc1"
+              alt="arrow"
+              height="24"
+              width="24"
+            />
+          </button>
+
+          <transition name="fade">
+            <div
+              v-if="isDropdownOpen1"
+              class="absolute z-10 right-0 mt-2 w-48 bg-white rounded-md shadow-lg"
+            >
+              <ul class="py-1">
+                <li @click="toggleDropdown1">
+                  <NuxtLink
+                    to="aboutus"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >About Us</NuxtLink
+                  >
+                </li>
+                <li @click="toggleDropdown1">
+                  <NuxtLink
+                    to="ourteam"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >Our Team</NuxtLink
+                  >
+                </li>
+              </ul>
+            </div>
+          </transition>
+        </div>
+
+        <div class="relative">
+          <button @click="toggleDropdown2" class="button">
+            <NuxtLink class="sans text-lg transition-all hover:text-gray-500"
+              >What we do</NuxtLink
+            >
+            <NuxtImg
+              :src="NuxtImgSrc2"
+              alt="arrow"
+              height="24"
+              width="24"
+            />
+          </button>
+
+          <transition name="fade">
+            <div
+              v-if="isDropdownOpen2"
+              class="absolute z-10 right-0 mt-2 w-48 bg-white rounded-md shadow-lg"
+            >
+              <ul class="py-1">
+                <li @click="toggleDropdown2">
+                  <NuxtLink
+                    to="ECPM"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >ECPM</NuxtLink
+                  >
+                </li>
+                <li @click="toggleDropdown2">
+                  <NuxtLink
+                    to="GAS"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >Gas Distribution</NuxtLink
+                  >
+                </li>
+                <li @click="toggleDropdown2">
+                  <NuxtLink
+                    to="power"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >Power Distribution</NuxtLink
+                  >
+                </li>
+                <li @click="toggleDropdown2">
+                  <NuxtLink
+                    to="IDPM"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >IDPM</NuxtLink
+                  >
+                </li>
+              </ul>
+            </div>
+          </transition>
+        </div>
+
+        <button class="button">
+          <NuxtLink
+            class="sans text-lg transition-all hover:text-gray-500"
+            to="projects"
+            >Projects</NuxtLink
+          >
         </button>
         <button class="button">
-          <NuxtLink class="sans text-lg transition-all hover:text-gray-500" to="projects">Projects</NuxtLink>
-        </button>
-        <button class="button">
-          <NuxtLink class="sans text-lg transition-all hover:text-gray-500" to="sustainability">Sustainability</NuxtLink>
+          <NuxtLink
+            class="sans text-lg transition-all hover:text-gray-500"
+            to="sustainability"
+            >Sustainability</NuxtLink
+          >
         </button>
       </nav>
     </header>
@@ -80,6 +161,17 @@ const toggleDropdown2 = () => {
 
 .focus-visible\:ring-offset-0:focus-visible {
   --tw-ring-offset-width: 0px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  /* transition: opacity 0.3s ease; */
 }
 
 .input-tag {
