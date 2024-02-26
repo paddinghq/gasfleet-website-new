14a6f5945084c8cd6e0785303b015d72593e9f83
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2024-02-26 10:57:47 +0100

    changes to navdropdown

--- components/Header.vue
+++ components/Header.vue
@@ -1,22 +1,29 @@
 <script setup>
 import { ref } from "vue";
+const upArrowBlue = `<svg width="17" height="16" viewBox="0 0 17 16" fill="#205FAD" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.7046 4.8C8.23794 4.8 7.77127 4.97994 7.41794 5.33338L3.07127 9.68005C2.87794 9.87338 2.87794 10.1934 3.07127 10.3867C3.2646 10.5801 3.5846 10.5801 3.77794 10.3867L8.1246 6.04005C8.4446 5.72005 8.9646 5.72005 9.2846 6.04005L13.6313 10.3867C13.8246 10.5801 14.1446 10.5801 14.3379 10.3867C14.5313 10.1934 14.5313 9.87338 14.3379 9.68005L9.99127 5.33338C9.63794 4.97994 9.17127 4.8 8.7046 4.8Z" fill="#205FAD"/>
+    </svg>`;
 
-const NuxtImgSrc1 = ref("/down.png");
-const NuxtImgSrc2 = ref("/down.png");
+const upArrow = `<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M8.7046 4.8C8.23794 4.8 7.77127 4.97994 7.41794 5.33338L3.07127 9.68005C2.87794 9.87338 2.87794 10.1934 3.07127 10.3867C3.2646 10.5801 3.5846 10.5801 3.77794 10.3867L8.1246 6.04005C8.4446 5.72005 8.9646 5.72005 9.2846 6.04005L13.6313 10.3867C13.8246 10.5801 14.1446 10.5801 14.3379 10.3867C14.5313 10.1934 14.5313 9.87338 14.3379 9.68005L9.99127 5.33338C9.63794 4.97994 9.17127 4.8 8.7046 4.8Z" fill="#0A0A0A"/>
+</svg>`;
 
+const downArrow = `<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.7046 11.2C8.23794 11.2 7.77127 11.0201 7.41794 10.6667L3.07127 6.32005C2.87794 6.12672 2.87794 5.80672 3.07127 5.61338C3.2646 5.42005 3.5846 5.42005 3.77794 5.61338L8.1246 9.96005C8.4446 10.2801 8.9646 10.2801 9.2846 9.96005L13.6313 5.61338C13.8246 5.42005 14.1446 5.42005 14.3379 5.61338C14.5313 5.80672 14.5313 6.12672 14.3379 6.32005L9.99127 10.6667C9.63794 11.0201 9.17127 11.2 8.7046 11.2Z" fill="#0A0A0A"/>
+</svg>`;
 const isDropdownOpen1 = ref(false);
 const isDropdownOpen2 = ref(false);
-
 const toggleDropdown1 = () => {
-  isDropdownOpen1.value = !isDropdownOpen1.value;
-  isDropdownOpen2.value = false;
-  NuxtImgSrc1.value = isDropdownOpen1.value ? "/down.png" : "/up.png";
+  isDropdownOpen1.value = true;
+  if (isDropdownOpen1.value) {
+    isDropdownOpen2.value = false;
+  }
 };
-
 const toggleDropdown2 = () => {
-  isDropdownOpen1.value = false;
-  isDropdownOpen2.value = !isDropdownOpen2.value;
-  NuxtImgSrc2.value = isDropdownOpen2.value ? "/down.png" : "/up.png";
+  isDropdownOpen2.value = true;
+  if (isDropdownOpen2.value) {
+    isDropdownOpen1.value = false;
+  }
 };
 </script>
 
@@ -29,46 +36,59 @@ const toggleDropdown2 = () => {
         <div class="relative" @click="toggleDropdown1">
           <button
             class="text-base font-semibold bg-white border-none flex gap-[0.3rem] items-center"
+            :class="{ 'focus:text-[#205fad]': isDropdownOpen1 }"
+            @focus="isDropdownOpen1 = true"
+            @blur="isDropdownOpen1 = false"
           >
-            <span>Who we are</span>
+            <span :class="{ 'text-[#205fad]': isDropdownOpen1 }"
+              >Who we are</span
+            >
             <span
               class="mt-1"
-              v-html="isDropdownOpen1 ? upArrow : downArrow"
+              v-html="isDropdownOpen1 ? upArrowBlue : downArrow"
             ></span>
           </button>
           <div
             v-if="isDropdownOpen1"
-            class="absolute top-full left-[-3.4rem] mt-2 p-3 bg-white shadow-md rounded-md z-10 flex flex-col gap-1 w-[10rem]"
+            class="absolute top-[4.2rem] left-0 mt-2 p-8 bg-white shadow-md rounded-md z-10 flex flex-col gap-4 w-[15rem] text-sm font-normal"
           >
-            <NuxtLink to="/about">About Us</NuxtLink>
-            <NuxtLink to="/location">Our Team</NuxtLink>
+            <NuxtLink to="/Aboutus">About Us</NuxtLink>
+            <NuxtLink to="/Brand">Brand</NuxtLink>
+            <NuxtLink to="/Ourteam">Our Team</NuxtLink>
+            <NuxtLink to="/Ourmission">Our Mission</NuxtLink>
+            <NuxtLink to="/Ourvision">Our Vision</NuxtLink>
           </div>
         </div>
         <div class="relative" @click="toggleDropdown2">
           <button
             class="text-base font-semibold bg-white border-none flex gap-[0.3rem] items-center"
+            :class="{ 'focus:text-[#205fad]': isDropdownOpen2 }"
+            @focus="isDropdownOpen2 = true"
+            @blur="isDropdownOpen2 = false"
           >
-            <span>What we do</span>
+            <span :class="{ 'text-[#205fad]': isDropdownOpen2 }"
+              >What we do</span
+            >
             <span
               class="mt-1"
-              v-html="isDropdownOpen2 ? upArrow : downArrow"
+              v-html="isDropdownOpen2 ? upArrowBlue : downArrow"
             ></span>
           </button>
           <div
             v-if="isDropdownOpen2"
-            class="absolute top-full left-0 mt-2 bg-white shadow-md rounded-md"
+            class="absolute top-[4.2rem] left-0 mt-2 bg-white p-3 shadow-md rounded-md z-10 flex flex-col gap-4 w-[15rem] text-sm font-normal"
           >
             <NuxtLink to="/mission">Mission</NuxtLink>
             <NuxtLink to="/vision">Vision</NuxtLink>
           </div>
         </div>
         <button
-          class="text-base font-semibold bg-white border-none flex gap-[0.5rem] items-center"
+          class="text-base font-semibold bg-white border-none flex gap-[0.5rem] items-center focus:text-[#205fad]"
         >
           <NuxtLink class="">Projects</NuxtLink>
         </button>
         <button
-          class="text-base font-semibold bg-white border-none flex gap-[0.5rem] items-center"
+          class="text-base font-semibold bg-white border-none flex gap-[0.5rem] items-center focus:text-[#205fad]"
         >
           <NuxtLink class="">Sustainability </NuxtLink>
         </button>
