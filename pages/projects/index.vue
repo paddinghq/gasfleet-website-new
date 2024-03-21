9c80217e32c24e5d608cf012a9ef5dc902b92ee7
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-03-21 02:02:38 +0100

    fix: project carousel on the homepage now linked

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,6 +3,7 @@ import All from "@/components/All.vue";
 import Completed from "@/components/Completed.vue";
 import Ongoing from "@/components/Ongoing.vue";
 import { ref, computed } from "vue";
+
 const ImgSrc1 = ref("hero.jpg");
 const rectangleImg = ref("rectangle.png");
 
